25026b0620199575884c38a9c6bcf88a104dd162
Adriano-bertellini <135441385+Adriano-bertellini@users.noreply.github.com>
2025-11-12 07:48:06 -0300

    Atualização - Projeto

--- Projeto.dpr
+++ Projeto.dpr
@@ -2,13 +2,15 @@ program Projeto;
 
 uses
   Vcl.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
+  uFormMain in 'uFormMain.pas' {Form1},
+  uFuncoes in 'uFuncoes.pas',
+  uModel in 'uModel.pas';
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFormMain, FormMain);
   Application.Run;
 end.
